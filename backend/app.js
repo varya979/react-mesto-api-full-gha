ad7d36e94c61cb1bf0c57ed49f1e0f8f5bbf81d4
varya979 <varya979@yandex.ru>
2023-06-13 19:03:57 +0300

    исправляет адрес монгоДБ

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ const { PORT = 3000 } = process.env;
 const app = express();
 app.use(cors());
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
 });
 
